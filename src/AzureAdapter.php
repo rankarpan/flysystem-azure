03cd937985463f673cd2add66283b6f2e4e872f1
Arpan Rank <rankarpan@hotmail.com>
2017-03-05 10:13:21 +0530

    League Azure Storage With signedIP v1.0.9

--- src/AzureAdapter.php
+++ src/AzureAdapter.php
@@ -501,11 +501,12 @@ class AzureAdapter extends AbstractAdapter
     {
         if (is_null($signedIP)) {
             $signedIP = request()->ip();
-
-            if (preg_match('/(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/', $signedIP)) {
-                return '';
-            }
         }
+
+        if (preg_match('/(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/', $signedIP)) {
+            $signedIP = '';
+        }
+
         return $signedIP;
     }
 }
